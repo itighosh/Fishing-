ac8f7d1ca2a229b5d167201bf1df435ea357d9e5
Iti Ghosh <126597220+itighosh@users.noreply.github.com>
2026-07-14 18:17:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var swiper =new Swiper('.mySwiper',{
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    pagination:{
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
